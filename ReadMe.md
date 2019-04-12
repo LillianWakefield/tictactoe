f571c7c40c90b64f8caa11cca33cefa71c3eeb04
Lillian Wakefield <lillianwakefield@gmail.com>
2019-04-11 17:17:32 -0700

    Style changes to front and back end. Added ReadMe

---
+++ ReadMe.md
@@ -0,0 +1,18 @@
+<h2>Tic Tac Toe</h2>
+<p>This is a simple Tic Tac Toe game built using jQuery..</P>
+
+<h4>Instructions</h4>
+<p>Because this game is built using JavaScript, just follow this link to see it in action:<br>
+ <ahref="https://lillianwakefield.github.io/tictactoe/">https://lillianwakefield.github.io/tictactoe/</a></p>
+
+<h4>Build WIth</h4>
+<ul>
+<li>CSS</li>
+<li>HTML</li>
+<li>JavaScript</li>
+<li><a href ="https://getbootstrap.com/docs/4.1/getting-started/introduction/">Bootstrap</a></li>
+<li><a href="https://jquery.com/">jQuery</a></li>
+</ul>
+
+<h4>Author</h4>
+<li>Lillian Wakefield</li>
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Defines global variables: players, header messages, and initial game state
-const player1 = 'Player 1';
-const player2 = 'Player 2';
+const player1 = 1;
+const player2 = 2;
 
 const ready1 = 'Ready, Player 1?';
 const ready2 = 'Ready, Player 2?';
@@ -14,14 +14,17 @@ let turnCounter = 0;
 
 //Game play triggered by clicking on board
 function selectSpace(){
-    const whoseTurn = document.querySelector('.whoseTurnIsItAnyway').innerHTML
+    const whoseTurn = document.querySelector('.whoseTurnIsItAnyway').textContent;
 
     //Checks if game is still active and if game square is unoccupied
-    if(this.innerHTML === '' && ((whoseTurn === ready1 )||(whoseTurn === ready2))){
+    if(this.textContent === '' && (whoseTurn === ready1 ||whoseTurn === ready2)){
         turnCounter +=1;
+        if(turnCounter === 1){
+            $('.instructions').css("visibility", "hidden");
+        };
         //Updates header based on current player and toggles player for next turn   
         if(currentPlayer === player1){
-            this.innerHTML = 'X';
+            this.textContent = 'X';
             currentPlayer = player2;
             if(turnCounter <9 ){
                 updateHeader(ready2);
@@ -29,32 +32,44 @@ function selectSpace(){
             //Updates header for end game in case of draw
             else{
                 updateHeader(winCat);
-            }
+            };
         }
         else if (currentPlayer === player2) {
-            this.innerHTML = 'O';
+            this.textContent = 'O';
             currentPlayer = player1;
             updateHeader(ready1);
-    }
+    };
         //Checks for win conditions each turn
         checkWinConditions();
-    }
-}
+    };
+};
 
 //Updates header based on gameplay
 function updateHeader(message){
-    $('.whoseTurnIsItAnyway').html(message);
-}
+    $('.whoseTurnIsItAnyway').text(message);
+    if(message === win1 || message === win2){
+        $('.whoseTurnIsItAnyway').addClass('blinkyVictory');
+    };
+};
 
 //Checks for win conditions each turn
 function checkWinConditions(){
     //Creates array with current game state each turn
-    let inner = []
-    let squaresCheck = document.querySelectorAll('.space')
-    squaresCheck.forEach(square => inner.push(square.innerHTML));
+    const inner = [];
+    const squaresCheck = document.querySelectorAll('.space');
+    squaresCheck.forEach(square => inner.push(square.textContent));
 
     //Defines win conditions
-    let winCons = [inner.slice(0,3).join(''), inner.slice(3,7).join(''), inner.slice(6).join(''), inner[0]+inner[3]+inner[6], inner[1]+inner[4]+inner[7], inner[2]+inner[5]+inner[8], inner[0]+inner[4]+inner[8], inner[2]+inner[4]+inner[6]]
+    const winCons = [
+        inner.slice(0,3).join(''), 
+        inner.slice(3,6).join(''), 
+        inner.slice(6).join(''), 
+        inner[0]+inner[3]+inner[6], 
+        inner[1]+inner[4]+inner[7], 
+        inner[2]+inner[5]+inner[8], 
+        inner[0]+inner[4]+inner[8], 
+        inner[2]+inner[4]+inner[6]
+    ];
     
     //Checks for win conditions and updates header
     winCons.forEach(winCon => {
@@ -63,12 +78,12 @@ function checkWinConditions(){
         }
         else if (winCon ==='OOO'){
             return updateHeader(win2);
-        }
-    })
-}
+        };
+    });
+};
 
 //Get's things started and adds click listener to each board space
 $(document).ready(function(){
-    const squaresReady = document.querySelectorAll('.space')
+    const squaresReady = document.querySelectorAll('.space');
     squaresReady.forEach(square => square.addEventListener('click', selectSpace));
-})
+});
